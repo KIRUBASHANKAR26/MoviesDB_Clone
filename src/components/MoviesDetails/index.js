5ed5398bfefbf87b9aef09052464ec2b4ff62160
KIRUBASHANKAR26 <48342585+KIRUBASHANKAR26@users.noreply.github.com>
2021-07-23 13:01:04 +0530

    added,cast and social media

--- src/components/MoviesDetails/index.js
+++ src/components/MoviesDetails/index.js
@@ -7,6 +7,7 @@ import {CircularProgressbar} from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import ReactPlayer from "react-player";
 import Topbar from '../topbar';
+import NumberFormat from 'react-number-format';
 
 
 
@@ -16,6 +17,8 @@ const Moviesdetails = () => {
     const [youtubeKey, setYoutubeKey] = useState([])
     const [trailerPlayer, settrailerPlayer] = useState(false)
     const [casts, setCasts] = useState([])
+    const [count, setCount] = useState(12)
+    const [socialMedia, setSocialMedia] = useState([])
 
      useEffect(() => {
         axios(`${moviesDB_URL}/movie/${movieid}?api_key=${API_KEY}&language=en-US`)
@@ -24,15 +27,20 @@ const Moviesdetails = () => {
 
         //YouTube key
         axios(`${moviesDB_URL}/movie/${movieid}/videos?api_key=${API_KEY}`)
-        .then((res) => {setYoutubeKey(res.data.results[0].key)
-        })
+        .then((res) => setYoutubeKey(res.data.results[0].key))
         .catch((err)=> console.log(err));
 
         //cast and crew
 
         axios(`${moviesDB_URL}/movie/${movieid}/credits?api_key=${API_KEY}`)
-        .then((res) => setCasts(res.data.cast))
+        .then((res) =>setCasts(res.data.cast))
+        .catch((err)=> console.log(err))
+        
 
+        //social media
+        axios(`${moviesDB_URL}/movie/${movieid}/external_ids?api_key=${API_KEY}`)
+        .then((res) =>setSocialMedia(res.data))
+        .catch((err)=> console.log(err))
      }, [])
 
      ///-----------Convert the time, Minutes to hour
@@ -49,6 +57,9 @@ const Moviesdetails = () => {
     }
     const closeButton = () => {
         settrailerPlayer(trailerPlayer)
+    }
+    const addmore = () => {
+        setCount(count+12);
     }
     
 
@@ -136,11 +147,54 @@ const Moviesdetails = () => {
                 </div>
             </div>
         }
-        {/* <div>
-            {
-                casts.map((cast) => console.log(cast))
-            }
-        </div> */}
+        <div style={{display:"flex",margin:"1rem 0"}}>
+            <div style={{maxWidth:"70%"}}>
+            <h4 style={{margin:"0 7% 1rem", fontSize:"2rem"}}>Top Billed Cast</h4>
+            { <div className="scrollbar" style={{display: "flex",gap: "1rem",overflowX: "auto",margin: "0rem 7%",padding: "1rem 0"}}>
+                {
+                    casts.slice(0,count).map((cast,index) => 
+                    <div key={index} style={{boxShadow: "0 2px 8px rgb(0 0 0 / 10%)",display: "inline-block",borderRadius: "1rem"}}>
+                        <img style={{width:"100px", width: "140px",objectFit: "cover",height: "170px",objectPosition: "top center",borderRadius:"1rem 1rem 0 0"}} 
+                        onError={(e)=>{e.target.onerror = null; e.target.src="https://www.hhcenter.org/wp-content/uploads/2017/02/person-placeholder.jpg"}}
+                        src={`${IMG_PATH}/${cast.profile_path}`} title={cast.name} alt={cast.name}/>
+                        <div className="profile-info">
+                            <p style={{fontWeight: "800",marginBottom: "0.5rem"}}>{cast.name}</p>
+                            <p style={{fontSize: "0.8rem"}}>{cast.character}</p>
+                        </div>
+                    </div>)
+                }
+                <button style={{backgroundColor: "transparent", border:"none",textDecoration:"underline",cursor:"pointer",outline:"none"}} onClick={addmore}>View more</button>
+
+            </div> }
+            </div>
+            <div>
+                <div className="social-media-wrapper" style={{display:"flex",gap:"1rem",marginBottom:"1.5rem"}}>
+                    <span><a href={`https://www.facebook.com/${socialMedia.facebook_id}`} rel="noopener noreferrer" target="_blank"><i style={{fontSize:"2rem",cursor:"pointer"}} class="fab fa-facebook-square"></i></a></span>
+                    <span><a href={`https://twitter.com/${socialMedia.twitter_id}`} rel="noopener noreferrer" target="_blank"><i style={{fontSize:"2rem",cursor:"pointer"}} class="fab fa-twitter"></i></a></span>
+                    <span><a href={`https://www.instagram.com/${socialMedia.instagram_id}`} rel="noopener noreferrer" target="_blank"><i style={{fontSize:"2rem",cursor:"pointer",borderRight:"1px solid gray",paddingRight:"0.75rem"}} class="fab fa-instagram"></i></a></span>
+                    <span><a href={movieData.homepage} rel="noopener noreferrer" target="_blank"><i style={{fontSize:"2rem",transform: "rotate(45deg)",cursor:"pointer"}} class="fas fa-link"></i></a></span>
+                </div>
+                <div>
+                    {
+ 
+                        movieData && 
+                        <div >
+                            <h6>Status</h6>
+                            <p>{movieData.status}</p>
+                            <h6>Orginal Language</h6>
+                            <p>English</p>
+                            <h6>Budget</h6>
+                            <NumberFormat value={movieData.budget} displayType={'text'} thousandSeparator={true} prefix={'$'} />
+                            <h6 style={{marginTop:"0.5rem"}}>Revenue</h6>
+                            <NumberFormat value={movieData.revenue} displayType={'text'} thousandSeparator={true} prefix={'$'} />
+                        </div>
+                     
+                    }
+                </div>
+            </div>
+            {console.log(socialMedia)}
+        </div>
+        
     </div> );
 }
  
